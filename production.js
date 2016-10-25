4388261212e03177b240a951e9ea71daac6f4fea
Gar <gar@lonelydatu.com>
2016-10-24 22:18:45 -0400

    gar

--- production.js
+++ production.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use( express.static('dist'));
+app.use( '/dental', express.static('dist'));
 
 // // GET method route
 // app.get('/dental', function (req, res) {
